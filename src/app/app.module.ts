b9810c2efe699e0ef01b29bfb559a1c2b5f112e5
RiosAlejandro <alexandro.aa86@gmail.com>
2022-11-11 17:45:16 -0300

    avances en maquetacion y estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { AvatarComponent } from './components/elements/avatar/avatar.component';
-import { MenuComponent } from './components/elements/menu/menu.component';
+import { SectionComponent } from './components/section/section.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AvatarComponent,
-    MenuComponent
+    SectionComponent,
+    FooterComponent,
+    MainComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
